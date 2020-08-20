67be6b688e7f2faf788efb650dfb2f58f78a0324
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2020-08-20 18:49:20 +0300

    vuejs homework

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <input type="text" v-model='list'>
+  <button @click='save' v-show='show'>Save point</button>
+  
+  <ul>
+      <li v-for="(point, indexList) in todolist" :key='indexList'>
+        {{point}} 
+         <button @click='remove(indexList)'>Delete point</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      msg: 'Hello world!!!',
+      todolist:[],
+      saveBtn:0,
+      list:'',
+      show: true,
+    }
+  },
+
+  methods:{
+
+    save(){
+      this.todolist.push(this.list);
+      this.list='';
+      this.todolist.length>9 ? this.show=false : this.show=true;
+    },
+    remove(indexList){
+      this.todolist.splice(indexList,1);
+      this.todolist.length>9 ? this.show=false : this.show=true;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
